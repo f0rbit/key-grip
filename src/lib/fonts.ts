525c5a0f5be6dd68e269e92571d1ecbd6b91c9b1
f0rbit <dev@forbit.dev>
2024-12-22 20:16:23 +1030

    feat: load fonts into next

---
+++ src/lib/fonts.ts
@@ -0,0 +1,10 @@
+import localFont from "next/font/local";
+
+export const adramalech = localFont({ src: "../../public/adramalech.ttf" })
+export const albemarle = localFont({ src: "../../public/albemarle.ttf" })
+export const celticsea = localFont({ src: "../../public/celticsea.ttf" })
+export const durendal = localFont({ src: "../../public/durendal.ttf" })
+export const fuse = localFont({ src: "../../public/fuse.otf" })
+export const minima = localFont({ src: "../../public/minima.ttf" })
+export const norumbega = localFont({ src: "../../public/norumbega.ttf" })
+export const scurlock = localFont({ src: "../../public/scurlock.ttf" })
