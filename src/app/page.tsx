c83d1ee80d31828230033cf902e4b6d0991e2d38
f0rbit <dev@forbit.dev>
2025-01-22 12:31:02 +1030

    feat: move ep links into menu bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,14 +25,6 @@ export default function Home() {
       {/* <h1>Key Grip</h1> */}
       <FontSwitcher text={text} fonts={fonts} interval={interval} />
 
-
-      <section className="absolute bottom-6 left-6 grid gap-2">
-        <EPLink label="NEW EP" href="/ep2" year="2024" />
-        <EPLink label="Key Grip...?" href="/ep1" year="2023" />
-        <br />
-
-      </section>
-
       <section className="absolute bottom-6 right-6 grid gap-2">
         <SocialIcons direction="row" />
       </section>
@@ -40,14 +32,3 @@ export default function Home() {
   );
 }
 
-function EPLink({ label, href, year }: Readonly<{ label: string; href: string; year: string }>) {
-
-  return (
-    <Link href={href} className="flex flex-row items-center gap-2 hover:scale-110 transition-all duration-300 origin-left group">
-      <h3 className="transition-all duration-300 text-2xl font-bold text-neutral-800 dark:text-neutral-200 dark:group-hover:text-neutral-50">
-        {label}
-      </h3>
-      <p className="transition-all duration-300 text-neutral-500 dark:text-neutral-400 dark:group-hover:text-neutral-300">{year}</p>
-    </Link>
-  );
-}
