2abf5006180526475a0fe150504bf312de9deefc
f0rbit <dev@forbit.dev>
2025-01-23 22:03:14 +1030

    feat: skip to 2 seconds in forest video

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+"use client";
+import React, { useEffect } from 'react';
 
 interface VideoBackgroundProps {
   videoPath?: string;
@@ -7,6 +8,12 @@ interface VideoBackgroundProps {
 const VideoBackground: React.FC<VideoBackgroundProps> = ({ 
   videoPath = '/videos/walkingVid.mp4'  // Default path, update this to match your video location
 }) => {
+  // start it at 10 seconds in
+  useEffect(() => {
+    const video = document.querySelector('video')!;
+    video.currentTime = 2;
+  }, []);
+
   return (
     <div className="fixed top-0 left-0 w-screen h-screen -z-10 overflow-hidden">
       <video
