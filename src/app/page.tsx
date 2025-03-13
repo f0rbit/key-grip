6c1e2241186d2a933674c2653cfaa3c3ede0dd5e
CamBlackwell <cameronblackwell89@gmail.com>
2025-03-13 13:33:38 +1030

    feat: added blur to video and removed unneded imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { BurgerBar } from "@/components/burger-bar";
-import WindChimes from "@/components/chimes";
 import FontSwitcher from "@/components/font-switcher";
 import { SocialIcons } from "@/components/socials";
 import VideoBackground from '@/components/VideoBackground';
 import { adramalech, albemarle, celticsea, durendal, fuse, minima, norumbega, scurlock } from "@/lib/fonts";
-import Link from "next/link";
 
 export default function Home() {
   const fonts = [
@@ -24,6 +22,7 @@ export default function Home() {
     <div className="flex w-screen h-screen justify-center items-center relative text-neutral-50">
       <BurgerBar />
       <VideoBackground videoPath="/videos/walkingVid.mp4" />
+      <div className="fixed inset-0 backdrop-blur-sm z-[0] top-0 left-0" />
 
       <div className="absolute inset-0 bg-black/10 -z-10"></div>
       <FontSwitcher text={text} fonts={fonts} interval={interval} />
