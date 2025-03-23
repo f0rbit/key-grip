df548cdf73467a86b6abaff2985f0390f1c85c9d
f0rbit <dev@forbit.dev>
2025-03-23 16:20:58 +1030

    feat: make the spotify & instagram logos bigger

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -52,7 +52,7 @@ const MusicianLandingPage = () => {
             <h1 className="text-3xl font-bold mb-2 text-white">Key Grip</h1>
           </div>
 
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-4 text-[7vw] sm:text-base">
             {links.map((link) => (
               <button
                 key={link.title}
